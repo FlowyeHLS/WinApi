bd459b3af2d2bee26c24b9814be4d50e435f7678
User <User@DESKTOP-6KNLMQJ>
2025-06-23 18:30:30 +0500

    KeyBoard in progress

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -255,11 +255,58 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			case IDC_BUTTON_MINUS:a -= b; break;
 			case IDC_BUTTON_ASTER:a *= b; break;
 			case IDC_BUTTON_SLASH:a /= b; break;
-
 			}
 			input_operation = FALSE;
 			sprintf(szDisplay, "%g", a);
 			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)szDisplay);
+		}
+	}
+	break;
+	case WM_KEYUP:
+	{
+
+		switch (wParam)
+		{
+		default:
+			if (wParam >= '0' && wParam <= '9')
+			{
+				SendMessage(hwnd, WM_COMMAND, LOWORD(wParam - '0' + IDC_BUTTON_0), 0);
+				SendMessage(GetDlgItem(hwnd, LOWORD(wParam) - '0' + IDC_BUTTON_0), BM_SETSTATE, FALSE, 0);
+			}
+		}
+	}
+	break;
+
+	case WM_KEYDOWN:
+	{
+		//MessageBox(hwnd, "Pressed", "Info", MB_OK);
+		if (GetKeyState(VK_SHIFT) < 0)
+			if (wParam == 0x38)
+			{
+				SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_ASTER, 0);
+				SendMessage(GetDlgItem(hwnd,IDC_BUTTON_ASTER),BM_GETSTATE,TRUE, 0);
+			}
+			else
+				if (wParam >= '0' && wParam <= '9')
+				{
+					SendMessage(GetDlgItem(hwnd, LOWORD(wParam) - '0' + IDC_BUTTON_0), BM_SETSTATE, TRUE, 0);
+				}
+
+		switch (wParam)
+		{
+		default:
+			break;
+		case VK_OEM_PERIOD: SendMessage(GetDlgItem(hwnd, IDC_BUTTON_POINT), BM_SETSTATE, TRUE, 0); break;
+		case VK_DECIMAL:	SendMessage(GetDlgItem(hwnd, IDC_BUTTON_PLUS), BM_SETSTATE, TRUE, 0); break;
+		case VK_ADD:		SendMessage(GetDlgItem(hwnd, IDC_BUTTON_PLUS), BM_SETSTATE, TRUE, 0); break;
+		case VK_OEM_PLUS:   SendMessage(GetDlgItem(hwnd, IDC_BUTTON_PLUS), BM_SETSTATE, TRUE, 0); break;
+		case VK_OEM_MINUS:  SendMessage(GetDlgItem(hwnd, IDC_BUTTON_MINUS), BM_SETSTATE, TRUE, 0); break;
+		case VK_MULTIPLY:   SendMessage(GetDlgItem(hwnd, IDC_BUTTON_ASTER), BM_SETSTATE, TRUE, 0); break;
+		case VK_DIVIDE:		SendMessage(GetDlgItem(hwnd, IDC_BUTTON_SLASH), BM_SETSTATE, TRUE, 0); break;
+		case VK_OEM_2:		SendMessage(GetDlgItem(hwnd, IDC_BUTTON_PLUS), BM_SETSTATE, TRUE, 0); break;
+		case VK_BACK:		SendMessage(GetDlgItem(hwnd, IDC_BUTTON_BSP), BM_SETSTATE, TRUE, 0); break;
+		case VK_ESCAPE:		SendMessage(GetDlgItem(hwnd, IDC_BUTTON_CLR), BM_SETSTATE, TRUE, 0); break;
+		case VK_RETURN:		SendMessage(GetDlgItem(hwnd, IDC_BUTTON_EQUAL), BM_SETSTATE, TRUE, 0); break;
 		}
 	}
 	break;
